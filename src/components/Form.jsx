5434c21b493584067455782b0f28cf17804b3391
ENStudio <esi.enstudio@gmail.com>
2024-08-26 12:46:32 +0600

    Remove useState and implement useRef

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,21 +1,18 @@
-import {useState} from "react";
+import {useRef} from "react";
 import {IoMdAdd} from "react-icons/io";
-
 function Form({ getNotes }) {
 
-    const [inputValue, setInputValue] = useState({ title:'', description:'' });
-
-    const handleInputValue = ( property, value ) => {
-        setInputValue(prevState => ({
-            ...prevState,
-            [property]: value
-        }))
-    }
+    const noteTitle = useRef();
+    const noteDescription = useRef();
 
     const noteSubmit = (e) => {
         e.preventDefault()
-        getNotes( inputValue );
-        setInputValue({title:'', description: ''})
+
+        const title = noteTitle.current.value;
+        const description = noteDescription.current.value;
+        noteTitle.current.value = '';
+        noteDescription.current.value = '';
+        getNotes({title: title, description: description});
     }
 
     return (
@@ -24,9 +21,9 @@ function Form({ getNotes }) {
                 {/*Title*/}
                 <div className="max-w-sm">
                     <label htmlFor="input-label" className="block text-sm font-medium mb-2 dark:text-white">Title</label>
-                    <input type="text" id="input-label"
-                           onChange={ (e) => { handleInputValue('title', e.target.value) } }
-                           value={ inputValue.title }
+                    <input type="text"
+                           id="input-label"
+                           ref={noteTitle}
                            className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
                            placeholder="e.g. Water Bill..."/>
                 </div>
@@ -36,18 +33,12 @@ function Form({ getNotes }) {
                     <label htmlFor="textarea-label"
                            className="block text-sm font-medium mb-2 dark:text-white">Description</label>
                     <textarea id="textarea-label"
-                              onChange={ (e) => { handleInputValue('description', e.target.value) } }
-                              value={ inputValue.description }
+                              ref={noteDescription}
                               className="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" rows="1" placeholder="Detail's about notes..."></textarea>
                 </div>
 
                 {/*Add Button*/}
                 <div className="flex items-end">
-                    {/*<button type="submit"*/}
-                    {/*        className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-full border border-transparent dark:bg-gray-700 bg-blue-600 dark:hover:bg-gray-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">*/}
-                    {/*    <IoMdAdd />*/}
-                    {/*</button>*/}
-
                     <button type="submit"
                             className="flex shrink-0 justify-center items-center gap-2 w-[80px] h-[46px] text-2xl font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">
                         <IoMdAdd />
